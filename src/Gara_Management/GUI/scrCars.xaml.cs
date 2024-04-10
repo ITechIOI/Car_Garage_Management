6c934a7ee82ad6e8efb60c73e0c3382c8192735f
huyhoang462 <22520462@gm.uit.edu.vn>
2024-04-10 22:48:24 +0700

    them phieu tiep nhan sua chua

--- src/Gara_Management/GUI/scrCars.xaml.cs
+++ src/Gara_Management/GUI/scrCars.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gara_Management.GUI.Item;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,38 @@ namespace Gara_Management.GUI
     /// </summary>
     public partial class scrCars : UserControl
     {
+        Color color3 = (Color)ColorConverter.ConvertFromString("#5790AB");
+        Color color4 = (Color)ColorConverter.ConvertFromString("#064469");
+        Color color5 = (Color)ColorConverter.ConvertFromString("#072D44");
         public scrCars()
         {
             InitializeComponent();
+            for (int i = 0; i < 9; i++)
+            {
+                itCar itCar1 = new itCar();
+                ds_xe.Children.Add(itCar1);
+              
+            }
+        }
+        private void bd_exit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bd_exit.Background = new SolidColorBrush(color4);
+        }
+
+        private void bd_exit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bd_exit.Background = new SolidColorBrush(color3);
+        }
+
+        private void bd_exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Bạn có muốn thoát ứng dụng?", "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // Kiểm tra xem người dùng đã chọn Yes hay không
+            if (result == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
         }
     }
 }
