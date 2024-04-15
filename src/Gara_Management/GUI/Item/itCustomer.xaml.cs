24f3caa7a0ac1ea93266b5b301ccbc16a2271833
huyhoang462 <22520462@gm.uit.edu.vn>
2024-04-15 11:35:33 +0700

    basci  customer

---
+++ src/Gara_Management/GUI/Item/itCustomer.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gara_Management.GUI.Item
+{
+    /// <summary>
+    /// Interaction logic for itCustomer.xaml
+    /// </summary>
+    public partial class itCustomer : UserControl
+    {
+        public itCustomer()
+        {
+            InitializeComponent();
+        }
+
+        // Hiển thị bảng thông tin khách hàng
+        private void bd_customer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
