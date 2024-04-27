c8cef3613c2c5d858ef06f41deba459898ac44b0
NotASleeper <22520596@gm.uit.edu.vn>
2024-04-27 10:35:03 +0700

    Update screen Detail of Staff's Info

---
+++ src/Gara_Management/GUI/scrAccDetail.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gara_Management.GUI
+{
+    /// <summary>
+    /// Interaction logic for scrAccDetail.xaml
+    /// </summary>
+    public partial class scrAccDetail : UserControl
+    {
+        Color color3 = (Color)ColorConverter.ConvertFromString("#5790AB");
+        Color color4 = (Color)ColorConverter.ConvertFromString("#064469");
+        Color color5 = (Color)ColorConverter.ConvertFromString("#072D44");
+        public scrAccDetail()
+        {
+            InitializeComponent();
+        }
+        private void bd_exit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bd_exit.Background = new SolidColorBrush(color4);
+        }
+
+        private void bd_exit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bd_exit.Background = new SolidColorBrush(color3);
+        }
+        // nút thoát ứng dụng
+        private void bd_exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Bạn có muốn thoát ứng dụng?", "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // Kiểm tra xem người dùng đã chọn Yes hay không
+            if (result == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
+        }
+
+        private void bt_addAcc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void bt_return_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void bt_update_info_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void bt_change_pass_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
