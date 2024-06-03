e7f69ed80e2e5af6eb8ed05e5f286d9e845fa648
NotASleeper <22520596@gm.uit.edu.vn>
2024-06-03 23:10:52 +0700

    add card view info component

--- src/Gara_Management/GUI/Item/itSupplies.xaml.cs
+++ src/Gara_Management/GUI/Item/itSupplies.xaml.cs
@@ -44,5 +44,11 @@ namespace Gara_Management.GUI.Item
             crdStockIn.ShowDialog();
 
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            cardComponentDetail componentDetail=new cardComponentDetail();
+            componentDetail.ShowDialog();
+        }
     }
 }
