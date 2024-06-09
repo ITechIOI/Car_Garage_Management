5ce6042ea95571ed1a6a2c2328af4a1b711ab1fa
huyhoang462 <22520462@gm.uit.edu.vn>
2024-06-09 19:07:12 +0700

    crdComponent

---
+++ src/Gara_Management/GUI/Card/crdComponent.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Gara_Management.DAO;
+using Gara_Management.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gara_Management.GUI.Card
+{
+    /// <summary>
+    /// Interaction logic for crdComponent.xaml
+    /// </summary>
+    public partial class crdComponent : Window
+    {
+        public crdComponent()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void bd_exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void bd_save_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
