8f182cd30c8eafe72d1be438779660b4cd60fbc3
PhuongHo105 <22521160@gm.uit.edu.vn>
2024-06-10 23:34:14 +0700

    Update crdStockIn

--- src/Gara_Management/GUI/Card/crdComponent.xaml.cs
+++ src/Gara_Management/GUI/Card/crdComponent.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Gara_Management.DAO;
 using Gara_Management.DTO;
+using Gara_Management.GUI.Item;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +23,30 @@ namespace Gara_Management.GUI.Card
     /// </summary>
     public partial class crdComponent : Window
     {
+        private readonly grnInterface grn;
+
         string gara;
-        public crdComponent(string gara)
+        public crdComponent(string gara, grnInterface grnInterface)
         {
             InitializeComponent();
             tbx_delete.Text = "Hủy";
             this.gara = gara;
+            this.grn = grnInterface;
             LoadListComponentToComboBox();
+        }
+        public crdComponent(grnInterface grn, string gara, string id, string amount, string price)
+        {
+            InitializeComponent();
+            tbx_delete.Text = "Xóa";
+            tbx_save.Text = "Cập nhật";
+            this.gara = gara;
+            this.grn = grn;
+            LoadListComponentToComboBox();
+            cbx_component.SelectedItem = id;
+            cbx_component.IsEnabled = false;
+            bd_save.Visibility = Visibility.Collapsed;
+            txtb_amount.Text = amount;
+            txtb_price.Text = price;
         }
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -40,12 +59,54 @@ namespace Gara_Management.GUI.Card
         }
         private void bd_save_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (cbx_component.SelectedItem == null || txtb_amount.Text == "" || txtb_price.Text == "")
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin.", "Thông báo");
+            }
+            else
+            {
+                int amount;
+                if (!int.TryParse(txtb_amount.Text, out amount))
+                {
+                    MessageBox.Show("Số lượng phải là một số nguyên");
 
+                }
+                else
+                {
+                    int price;
+                    if (!int.TryParse(txtb_price.Text, out price))
+                    {
+                        MessageBox.Show("Đơn giá phải là một số nguyên");
+
+                    }
+                    else
+                    {
+                        string idCom = CarComponentDAO.Instance.GetComponentIDByName(gara, cbx_component.SelectedItem.ToString());
+                        grn.ReceivedData(idCom, price, amount);
+                        this.Close();
+                    }    
+                }
+            }
+            
         }
 
         private void bd_delete_MouseDown(object sender, MouseButtonEventArgs e)
         {
-              
+            if (tbx_delete.Text == "Xóa")
+            {
+                if (MessageBox.Show("Bạn có muốn xóa phụ tùng này trong danh sách nhập không?", "Thông báo", MessageBoxButton.YesNo)
+                    == MessageBoxResult.Yes)
+                {
+                    string idCom = CarComponentDAO.Instance.GetComponentIDByName(gara, cbx_component.SelectedItem.ToString());
+                    grn.ReceivedData(idCom, 0, 0);
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.Close();
+            }    
+
         }
         private void LoadListComponentToComboBox()
         {
@@ -59,6 +120,7 @@ namespace Gara_Management.GUI.Card
 
         private void txtb_price_TextChanged(object sender, TextChangedEventArgs e)
         {
+            bd_save.Visibility = Visibility.Visible;
             if (txtb_price.Text!="" && txtb_amount.Text != "")
             {
                 int price, amount;
--- src/Gara_Management/GUI/Item/itStockInDetail.xaml.cs
+++ src/Gara_Management/GUI/Item/itStockInDetail.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Gara_Management.DAO;
 using Gara_Management.DTO;
+using Gara_Management.GUI.Card;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -21,26 +22,33 @@ namespace Gara_Management.GUI.Item
     /// <summary>
     /// Interaction logic for itStockInDetail.xaml
     /// </summary>
-    public partial class itStockInDetail : UserControl
+    public partial class itStockInDetail : UserControl, grnInterface
     {
         public GRNDetail detail;
         string gara;
-        bool check = false;
-        public itStockInDetail(int i, string gara)
+        string id;
+
+
+        public itStockInDetail( int i, string gara, string id, int price, int amount)
         {
             InitializeComponent();
             txtb_orderednum.Text = i.ToString();
+            this.id = id;
             this.gara = gara;
-            check = true;
+            txtb_amount.Text = amount.ToString();
+            txtb_price.Text = price.ToString();
+            txtb_idStock.Text = id;
+            object nameCom = DataProvider.Instance.ExecuteScalar("SELECT NAME_COM from CAR_COMPONENTS WHERE ID_COM='" + id + "'");
+            txtb_name.Text = nameCom.ToString();
         }
-        public itStockInDetail(GRNDetail detail, string gara)
+        public itStockInDetail(GRNDetail detail, string gara, int i)
         {
             InitializeComponent();
             this.detail = detail;
             this.gara = gara;
+            this.id = detail.IDCom;
             txtb_orderednum.Text=this.detail.GRNOrdinalNum.ToString();
             txtb_idStock.Text = this.detail.IDCom;
-            check = true;
             txtb_price.Text =((int) this.detail.ComPrice).ToString();
             txtb_amount.Text=this.detail.ComQuantity.ToString();
             object nameCom = DataProvider.Instance.ExecuteScalar("SELECT NAME_COM from CAR_COMPONENTS WHERE ID_COM='" + this.detail.IDCom + "'");
@@ -51,34 +59,69 @@ namespace Gara_Management.GUI.Item
         private void txtb_amount_TextChanged(object sender, TextChangedEventArgs e)
         {
             int amount;
-            decimal price;
+            decimal price = 0;
             if (int.TryParse(txtb_amount.Text, out amount) && decimal.TryParse(txtb_price.Text, out price))
             {
                 txtb_sumofmoney.Text = (amount * price).ToString();
             }
-               
+
         }
 
         private void txtb_price_TextChanged(object sender, TextChangedEventArgs e)
         {
             int amount;
-            decimal price;
+            decimal price = 0;
             if (int.TryParse(txtb_amount.Text, out amount) && decimal.TryParse(txtb_price.Text, out price))
             {
                 txtb_sumofmoney.Text = (amount * price).ToString();
             }
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            crdComponent component = new crdComponent(this as grnInterface, gara, txtb_name.Text, txtb_amount.Text, txtb_price.Text);
+            component.bd_save.MouseDown += Bd_save_MouseDown;
+            if (this.detail != null)
+            {
+                component.bd_delete.Visibility = Visibility.Collapsed;
+                
+            }
+            else
+            {
+                component.bd_delete.MouseDown += Bd_delete_MouseDown;
+            }    
+            component.Show();
+        }
+
+        private void Bd_delete_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int j = -1;
+            foreach (UIElement item in ((Panel)this.Parent).Children)
+            {
+                if (item is itStockInDetail detail)
+                {
+                    if (detail.txtb_idStock.Text == this.txtb_idStock.Text)
+                    {
+                        j = int.Parse(detail.txtb_orderednum.Text);
+                    }
+                    if (j != -1 && int.Parse(detail.txtb_orderednum.Text) > j)
+                    {
+                        detail.txtb_orderednum.Text = (int.Parse(detail.txtb_orderednum.Text) - 1).ToString();
+                    }
+                }
+            }
+            ((Panel)this.Parent).Children.Remove(this);
+        }
+
+        private void Bd_save_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             
         }
 
-        private void txtb_idStock_TextChanged(object sender, TextChangedEventArgs e)
+        public void ReceivedData(string idCom, int price, int quantity)
         {
-            if (check && txtb_idStock.Text != null)
-            {
-                if (CarComponentDAO.Instance.GetCarComponentByID(txtb_idStock.Text, gara) != null)
-                    txtb_name.Text = CarComponentDAO.Instance.GetCarComponentByID(txtb_idStock.Text, gara).NameCom;
-                else
-                    MessageBox.Show("Phụ tùng không tồn tại.");
-            }
+            txtb_amount.Text = quantity.ToString();
+            txtb_price.Text = price.ToString();
         }
     }
 }
